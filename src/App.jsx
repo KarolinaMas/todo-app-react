aa1aa067779761e851769589703b62bf5edcf43b
Karolina Maščinskaitė <kmkarole@yahoo.com>
2025-07-04 14:37:24 +0300

    style list and div in the bottm of fthe list, create ListItem component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import moonIcon from "./assets/images/icon-moon.svg";
 import bgImage from "./assets/images/bg-image.jpg";
-import crossIcon from "./assets/images/icon-cross.svg";
-import checkIcon from "./assets/images/icon-check.svg";
+import ListItem from "./components/ListItem";
 
 const App = () => {
   return (
-    <div className="min-h-[100vh] bg-[#FAFAFA]">
+    <div className="min-h-[100vh] bg-[#FAFAFA] text-xs">
       <img
         src={bgImage}
         alt="img"
@@ -17,7 +16,7 @@ const App = () => {
         className="absolute inset-0 w-full h-[200px] sm:h-auto max-h-[35vh] bg-gradient-to-tl from-[#5596FF] to-[#AC2DEB] opacity-70 pointer-events-none"
         aria-hidden="true"
       />
-      <div className="w-full flex flex-col items-center px-6 -mt-40 mb-5">
+      <div className="w-full flex flex-col items-center px-6 -mt-40">
         <header className="flex justify-between items-center w-full max-w-xl mb-6 text-white z-10">
           <h1 className="text-2xl tracking-[0.5em] uppercase font-bold">
             todo
@@ -32,153 +31,24 @@ const App = () => {
           <input
             type="text"
             placeholder="Create a new todo..."
-            className="bg-white w-full max-w-xl p-4 rounded-sm text-xs placeholder:pl-9"
+            className="bg-white w-full max-w-xl p-4 rounded-sm placeholder:pl-9"
           />
         </div>
 
-        <ul className="w-full max-w-xl bg-white text-xs text-[#494C6B] mt-4 rounded-sm shadow-xs z-10">
-          <li className="relative w-full border-b border-[#E3E4F1] flex items-center justify-between p-4">
-            <label className="flex items-center w-full cursor-pointer relative">
-              <input
-                type="checkbox"
-                className="peer appearance-none w-5 h-5 border border-[#E3E4F1] rounded-full 
-                 checked:bg-gradient-to-br checked:from-[#55DDFF] checked:to-[#C058F3] 
-                 checked:border-none focus:outline-none relative"
-              />
-              <img
-                src={checkIcon}
-                alt="check icon"
-                className="absolute left-0 ml-[5px] h-[9px] pointer-events-none opacity-0 peer-checked:opacity-100"
-              />
-              <p className="pl-4 peer-checked:line-through peer-checked:text-[#D1D2DA]">
-                test
-              </p>
-            </label>
+        <section className="w-full max-w-xl bg-white  text-[#494C6B] mt-4 rounded-sm z-10 shadow-[0px_3px_38px_0px_rgba(148,149,165,0.35)]">
+          <ul className="w-full">
+            <ListItem />
+          </ul>
+          <div className="p-4 text-[#9495A5] flex justify-between w-full">
+            <p>5 items left</p>
+            <button>Clear Completed</button>
+          </div>
+        </section>
 
-            <button className="w-3 h-3 shrink-0">
-              <img src={crossIcon} alt="cross icon" />
-            </button>
-          </li>
-          <li className="relative w-full border-b border-[#E3E4F1] flex items-center justify-between p-4">
-            <label className="flex items-center w-full cursor-pointer relative">
-              <input
-                type="checkbox"
-                className="peer appearance-none w-5 h-5 border border-[#E3E4F1] rounded-full 
-                 checked:bg-gradient-to-br checked:from-[#55DDFF] checked:to-[#C058F3] 
-                 checked:border-none focus:outline-none relative"
-              />
-              <img
-                src={checkIcon}
-                alt="check icon"
-                className="absolute left-0 ml-[5px] h-[9px] pointer-events-none opacity-0 peer-checked:opacity-100"
-              />
-              <p className="pl-4 peer-checked:line-through peer-checked:text-[#D1D2DA]">
-                test
-              </p>
-            </label>
-
-            <button className="w-3 h-3 shrink-0">
-              <img src={crossIcon} alt="cross icon" />
-            </button>
-          </li>
-          <li className="relative w-full border-b border-[#E3E4F1] flex items-center justify-between p-4">
-            <label className="flex items-center w-full cursor-pointer relative">
-              <input
-                type="checkbox"
-                className="peer appearance-none w-5 h-5 border border-[#E3E4F1] rounded-full 
-                 checked:bg-gradient-to-br checked:from-[#55DDFF] checked:to-[#C058F3] 
-                 checked:border-none focus:outline-none relative"
-              />
-              <img
-                src={checkIcon}
-                alt="check icon"
-                className="absolute left-0 ml-[5px] h-[9px] pointer-events-none opacity-0 peer-checked:opacity-100"
-              />
-              <p className="pl-4 peer-checked:line-through peer-checked:text-[#D1D2DA]">
-                test
-              </p>
-            </label>
-
-            <button className="w-3 h-3 shrink-0">
-              <img src={crossIcon} alt="cross icon" />
-            </button>
-          </li>
-          <li className="relative w-full border-b border-[#E3E4F1] flex items-center justify-between p-4">
-            <label className="flex items-center w-full cursor-pointer relative">
-              <input
-                type="checkbox"
-                className="peer appearance-none w-5 h-5 border border-[#E3E4F1] rounded-full 
-                 checked:bg-gradient-to-br checked:from-[#55DDFF] checked:to-[#C058F3] 
-                 checked:border-none focus:outline-none relative"
-              />
-              <img
-                src={checkIcon}
-                alt="check icon"
-                className="absolute left-0 ml-[5px] h-[9px] pointer-events-none opacity-0 peer-checked:opacity-100"
-              />
-              <p className="pl-4 peer-checked:line-through peer-checked:text-[#D1D2DA]">
-                test
-              </p>
-            </label>
-
-            <button className="w-3 h-3 shrink-0">
-              <img src={crossIcon} alt="cross icon" />
-            </button>
-          </li>
-          <li className="relative w-full border-b border-[#E3E4F1] flex items-center justify-between p-4">
-            <label className="flex items-center w-full cursor-pointer relative">
-              <input
-                type="checkbox"
-                className="peer appearance-none w-5 h-5 border border-[#E3E4F1] rounded-full 
-                 checked:bg-gradient-to-br checked:from-[#55DDFF] checked:to-[#C058F3] 
-                 checked:border-none focus:outline-none relative"
-              />
-              <img
-                src={checkIcon}
-                alt="check icon"
-                className="absolute left-0 ml-[5px] h-[9px] pointer-events-none opacity-0 peer-checked:opacity-100"
-              />
-              <p className="pl-4 peer-checked:line-through peer-checked:text-[#D1D2DA]">
-                test
-              </p>
-            </label>
-
-            <button className="w-3 h-3 shrink-0">
-              <img src={crossIcon} alt="cross icon" />
-            </button>
-          </li>
-          <li className="relative w-full border-b border-[#E3E4F1] flex items-center justify-between p-4">
-            <label className="flex items-center w-full cursor-pointer relative">
-              <input
-                type="checkbox"
-                className="peer appearance-none w-5 h-5 border border-[#E3E4F1] rounded-full 
-                 checked:bg-gradient-to-br checked:from-[#55DDFF] checked:to-[#C058F3] 
-                 checked:border-none focus:outline-none relative"
-              />
-              <img
-                src={checkIcon}
-                alt="check icon"
-                className="absolute left-0 ml-[5px] h-[9px] pointer-events-none opacity-0 peer-checked:opacity-100"
-              />
-              <p className="pl-4 peer-checked:line-through peer-checked:text-[#D1D2DA]">
-                test
-              </p>
-            </label>
-
-            <button className="w-3 h-3 shrink-0">
-              <img src={crossIcon} alt="cross icon" />
-            </button>
-          </li>
-        </ul>
+        <footer className="text-center mt-10 text-sm text-gray-400">
+          Drag and drop to reorder list
+        </footer>
       </div>
-      <div className="px-6 mt-4">
-        <p>5 items left</p>
-        <button>clear completed</button>
-      </div>
-
-      <footer className="text-center mt-10 text-sm text-gray-400">
-        Drag and drop to reorder list
-      </footer>
     </div>
   );
 };
---
+++ src/components/ListItem.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import crossIcon from "../assets/images/icon-cross.svg";
+import checkIcon from "../assets/images/icon-check.svg";
+
+const ListItem = () => {
+  return (
+    <li className="relative w-full border-b border-[#E3E4F1] flex items-center justify-between p-4">
+      <label className="flex items-center w-full cursor-pointer relative">
+        <input
+          type="checkbox"
+          className="peer appearance-none w-5 h-5 border border-[#E3E4F1] rounded-full 
+                     checked:bg-gradient-to-br checked:from-[#55DDFF] checked:to-[#C058F3] 
+                     checked:border-none focus:outline-none relative"
+        />
+        <img
+          src={checkIcon}
+          alt="check icon"
+          className="absolute left-0 ml-[5px] h-[9px] pointer-events-none opacity-0 peer-checked:opacity-100"
+        />
+        <p className="pl-4 peer-checked:line-through peer-checked:text-[#D1D2DA]">
+          test
+        </p>
+      </label>
+
+      <button className="w-3 h-3 shrink-0">
+        <img src={crossIcon} alt="cross icon" />
+      </button>
+    </li>
+  );
+};
+
+export default ListItem;
